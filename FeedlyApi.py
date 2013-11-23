e1ff5831eede2745ecbb68cc4e94aa7fe9c823e9
km <km@37degree.com>
2013-11-23 17:21:34 +0800

    add topic tags search get api

--- FeedlyApi.py
+++ FeedlyApi.py
@@ -23,8 +23,13 @@ class FeedlyAPI:
         if data:
             data = urllib.urlencode(data)
         request = urllib2.Request(url, data,  headers)
-        result = urllib2.urlopen(request).read()
-        return result
+        try:
+            result = urllib2.urlopen(request).read()
+            return result 
+        except urllib2.HTTPError, e:
+            return e.read() 
+        except:
+            print traceback.format_exc()
 
     def getToken(self, code, redirect_uri, state=None):
         data = {
@@ -37,16 +42,11 @@ class FeedlyAPI:
         if state:
             data['state'] = state
         url = '/v3/auth/token'
-        try:
-            result = self.__request(url, data)
-            jsp_result = json.loads(result)
-            if 'access_token' in jsp_result:
-                self.token = jsp_result['access_token']
-            return jsp_result
-        except urllib2.HTTPError, e:
-            return json.loads(e.read())
-        except:
-            print traceback.format_exc()
+        result = self.__request(url, data)
+        jsp_result = json.loads(result)
+        if 'access_token' in jsp_result:
+            self.token = jsp_result['access_token']
+        return jsp_result
 
 
     def getSubscription(self, token=None):
@@ -81,9 +81,32 @@ class FeedlyAPI:
         jsp_result = json.loads(result)
         return jsp_result
 
-    
+    def getTopics(self, token):
+        if not token:
+            token = self.token
+        url = '/v3/topics'
+        result = self.__request(url, None, token)
+        jsp_result = json.loads(result)
+        return jsp_result
+
+    def getTags(self, token):
+        if not token:
+            token = self.token
+        url = '/v3/tags'
+        result = self.__request(url, None, token)
+        jsp_result = json.loads(result)
+        return jsp_result
+
+    def searchFeeds(self, keyword, token, number=20):
+        if not token:
+            token = self.token
+        url = '/v3/search/feeds?q=%s&n=%d' % (keyword, number)
+        result = self.__request(url, None, token)
+        jsp_result = json.loads(result)
+        return jsp_result
 
 if __name__ == '__main__':
     fa = FeedlyAPI('sandbox', 'Z5ZSFRASVWCV3EFATRUY')
     #print fa.getToken('AQAAqeZ7InUiOiIxMTA1MjE3ODMwNDgxNjc1NTI3MjAiLCJpIjoiMmU2MWZhMDUtYTA5Zi00MmU0LWFmNzctYzFjNjkyMDk4N2I5IiwicCI6NiwiYSI6IkZlZWRseSBzYW5kYm94IGNsaWVudCIsInQiOjEzODUxOTU4NDIwOTd9', 'http://localhost')
-    print fa.getCategories('AQAAk5J7ImkiOiIyZTYxZmEwNS1hMDlmLTQyZTQtYWY3Ny1jMWM2OTIwOTg3YjkiLCJwIjo2LCJhIjoiRmVlZGx5IHNhbmRib3ggY2xpZW50IiwidCI6MSwidiI6InNhbmRib3giLCJ4Ijoic3RhbmRhcmQiLCJlIjoxMzg1ODAwODQ1OTAwfQ:sandbox')
+    #print fa.getTags('AQAAk5J7ImkiOiIyZTYxZmEwNS1hMDlmLTQyZTQtYWY3Ny1jMWM2OTIwOTg3YjkiLCJwIjo2LCJhIjoiRmVlZGx5IHNhbmRib3ggY2xpZW50IiwidCI6MSwidiI6InNhbmRib3giLCJ4Ijoic3RhbmRhcmQiLCJlIjoxMzg1ODAwODQ1OTAwfQ:sandbox')
+    print fa.searchFeeds('apple', 'AQAAk5J7ImkiOiIyZTYxZmEwNS1hMDlmLTQyZTQtYWY3Ny1jMWM2OTIwOTg3YjkiLCJwIjo2LCJhIjoiRmVlZGx5IHNhbmRib3ggY2xpZW50IiwidCI6MSwidiI6InNhbmRib3giLCJ4Ijoic3RhbmRhcmQiLCJlIjoxMzg1ODAwODQ1OTAwfQ:sandbox')
